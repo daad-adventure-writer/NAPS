68ca04bb609f77b7730dd2c7feffae7afaea1b53
José Manuel Ferrer Ortiz <jmfo1982@gmail.com>
2022-02-19 09:04:00 +0100

    Commit inicial del editor de bases de datos gráficas
    De momento, sólo permite ver las imágenes.

---
+++ edita_bd_pics.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+# NAPS: The New Age PAW-like System - Herramientas para sistemas PAW-like
+#
+# Editor de bases de datos gráficas de DAAD
+# Copyright (C) 2022 José Manuel Ferrer Ortiz
+#
+# *****************************************************************************
+# *                                                                           *
+# *  This program is free software; you can redistribute it and/or modify it  *
+# *  under the terms of the GNU General Public License version 2, as          *
+# *  published by the Free Software Foundation.                               *
+# *                                                                           *
+# *  This program is distributed in the hope that it will be useful, but      *
+# *  WITHOUT ANY WARRANTY; without even the implied warranty of               *
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU         *
+# *  General Public License version 2 for more details.                       *
+# *                                                                           *
+# *  You should have received a copy of the GNU General Public License        *
+# *  version 2 along with this program; if not, write to the Free Software    *
+# *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  *
+# *                                                                           *
+# *****************************************************************************
+
+import math
+import sys
+
+try:
+  from PyQt4.QtCore import *
+  from PyQt4.QtGui  import *
+except:
+  from PyQt5.QtCore    import *
+  from PyQt5.QtGui     import *
+  from PyQt5.QtWidgets import *
+
+import graficos_daad
+
+from bajo_nivel import *
+
+
+class Ventana (QMainWindow):
+  """Ventana principal"""
+  def __init__ (self):
+    super (Ventana, self).__init__()
+    accSalir = QAction ('&Salir', self)
+    accSalir.setShortcut ('Ctrl+Q')
+    menuArchivo = self.menuBar().addMenu ('&Archivo')
+    menuArchivo.addAction (accSalir)
+    scroll = QScrollArea (self)
+    self.rejilla = QWidget (scroll)
+    self.rejilla.setLayout (QGridLayout (self.rejilla))
+    scroll.setWidget (self.rejilla)
+    accSalir.triggered.connect (self.close)
+    self.setCentralWidget (scroll)
+    self.setWindowTitle ('Editor de bases de datos gráficas')
+    self.showMaximized()
+
+
+def importaBD (nombreFichero):
+  """Importa una base de datos desde el fichero de nombre dado"""
+  global le
+  error = graficos_daad.carga_bd_pics (nombreFichero)
+  if error:
+    muestraFallo ('No se puede abrir el fichero:\n' + nombreFichero, error)
+    return
+
+  altoMax  = 0  # Alto  de imagen máximo
+  anchoMax = 0  # Ancho de imagen máximo
+  imagenes = []
+  for numImg in range (256):
+    recurso = graficos_daad.recursos[numImg]
+    if not recurso:
+      imagenes.append (None)
+      continue
+    paleta = []
+    for rojo, verde, azul in recurso['paleta']:
+      paleta.append (qRgb (rojo, verde, azul))
+    ancho, alto = recurso['dimensiones']
+    col    = 0  # Columna actual
+    fila   = 0  # Fila actual
+    imagen = QImage (ancho, alto, QImage.Format_Indexed8)
+    imagen.setColorTable (paleta)
+    for indicePaleta in recurso['imagen']:
+      imagen.setPixel (col, fila, indicePaleta)
+      col += 1
+      if col == ancho:
+        col   = 0
+        fila += 1
+    imagenes.append (imagen)
+    if alto > altoMax:
+      altoMax = alto
+    if ancho > anchoMax:
+      anchoMax = ancho
+
+  dtWidget  = QDesktopWidget()  # Para obtener el ancho de la pantalla (dado que el de la ventana no se correspondía con el real)
+  geometria = dtWidget.availableGeometry (ventana)
+  margen    = 8
+  columnas  = geometria.width() // (anchoMax + margen)
+  filas     = math.ceil (256. / columnas)
+  ventana.rejilla.setMinimumSize (geometria.width() - 20, (filas * (altoMax + margen) + ((filas + 1) * 6)))
+
+  col  = 0  # Columna actual
+  fila = 0  # Fila actual
+  for i in range (256):
+    imagen = imagenes[i]
+    if imagen:
+      pixmap = QPixmap     (imagen)
+      icono  = QIcon       (pixmap)
+      widget = QPushButton (icono, str (i))
+      widget.setIconSize (pixmap.rect().size())
+      # widget.setAlignment   (Qt.AlignCenter)
+      # widget.setPixmap      (pixmap)
+    else:
+      widget = QPushButton (str (i))
+    widget.setMinimumSize (anchoMax + margen, altoMax + margen)
+    ventana.rejilla.layout().addWidget (widget, fila, col)
+    col += 1
+    if col == columnas:
+      col   = 0
+      fila += 1
+
+def muestraFallo (mensaje, detalle, parent = None):
+  """Muestra un diálogo de fallo leve"""
+  # TODO: sacar a módulo común con el IDE
+  global dlg_fallo, ventana_principal
+  try:
+    dlg_fallo
+  except:  # Diálogo no creado aún
+    if parent == None:
+      parent = ventana_principal
+    dlg_fallo = QMessageBox (parent)
+    dlg_fallo.addButton ('&Aceptar', QMessageBox.AcceptRole)
+    dlg_fallo.setIcon (QMessageBox.Warning)
+    dlg_fallo.setWindowTitle ('Fallo')
+  dlg_fallo.setText (mensaje)
+  dlg_fallo.setInformativeText ('Causa:\n' + detalle)
+  dlg_fallo.exec_()
+
+
+aplicacion = QApplication (sys.argv)
+ventana    = Ventana()
+ventana_principal = ventana  # Para muestraFallo
+
+if len (sys.argv) > 1:
+  importaBD (sys.argv[1])
+
+sys.exit (aplicacion.exec_())
