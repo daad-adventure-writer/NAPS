ae59a1370a1bcc709b59d52578d6fc53be9eb479
José Manuel Ferrer Ortiz <jmfo1982@gmail.com>
2024-09-24 21:56:20 +0200

    Rellenar con espacios al final líneas completadas al imprimir
    Así se aplicará en su parte final también el color de fondo correspondiente.

--- gui_pygame.py
+++ gui_pygame.py
@@ -1284,6 +1284,9 @@ Si tiempo no es 0, esperará hasta ese tiempo en segundos cuando se espere tecla 
       imprime_linea (lineas[i], redibujar = redibujar, colores = colores)
     if i < len (lineas) - 1:  # Sólo si la línea se ha completado
       lineas_mas[elegida] += 1
+      if textoNormal and cursor[0] + len (lineas[i]) < tope[0]:  # Cabían más caracteres al final de la línea
+        cursor[0] += len (lineas[i])
+        imprime_linea (chr (16) * (tope[0] - cursor[0]))
   if lineas:  # Había alguna línea
     if cadena[-1] == '\n':  # La cadena terminaba en nueva línea
       if cursor[1] == tope[1] - 1:
